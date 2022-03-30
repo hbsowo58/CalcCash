5ca45ff21577de99da0876e18d80de116f46a0a1
hbs <hbsowo58@gmail.com>
2022-03-30 13:55:36 +0900

    feat : add mileage feat

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
 const tax = document.querySelector(".tax")
-const TAX = Number(tax.value)
+const money = document.querySelector(".money")
+const mileage = document.querySelector(".mileage")
+const buy_money = document.querySelector(".buy_money")
+const MILEAGE = 30
 
 let debounce = null;
 
-// const button = document.querySelector(".button")
-const money = document.querySelector(".money")
-
-money.addEventListener("keypress", function(){
-    const result = document.querySelector(".result");
-    const MONEY = Number(money.value)
-
-    result.textContent = MONEY - (TAX * MONEY)/100
-})
-
 money.addEventListener('keyup', keyUpFn);
+buy_money.addEventListener('keyup', keyUpFn);
+tax.addEventListener('click', keyUpFn);
+mileage.addEventListener('click', keyUpFn);
 
-function keyUpFn(el) {
-    console.log(el.target.value)
+function keyUpFn() {
     clearTimeout(debounce);
     debounce = setTimeout(() => {
+            const TAX = Number(tax.value)
             const result = document.querySelector(".result");
-            const MONEY = Number(el.target.value)
-            result.textContent = MONEY - (TAX * MONEY)/100
+            const MONEY = Number(money.value)
+            const BUY_MONEY = Number(buy_money.value)
+            const use_cash = document.querySelector(".use_cash")
+
+            if(mileage.checked){
+                const SALE_MONEY  = calc(BUY_MONEY, MILEAGE)
+                use_cash.textContent = SALE_MONEY
+            } else {
+                use_cash.textContent = BUY_MONEY
+                result.textContent = calc(MONEY, TAX)
+            }
         }, 0);
     
+}
+
+const calc = (MONEY, TAX) => {
+    return MONEY - (TAX * MONEY)/100
 }
